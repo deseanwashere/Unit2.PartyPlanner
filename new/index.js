93012bc4ecaa49bc05fcdcf3fa82ac6e89c180ba
Desean Richard <desean@Deseans-MacBook-Air.local>
2024-03-24 23:19:02 -0500

    SECOND

---
+++ new/index.js
@@ -0,0 +1,49 @@
+const API_URL =`https://fsa-crud-2aa9294fe819.herokuapp.com/api`
+
+const fetchAPI = async () => {
+try {
+  const response = await fetch(API_URL);
+  const dataObject = await response.json();
+
+}
+
+}
+
+const clickMe = document.querySelector("button1")
+console.log(clickMe)
+
+const deleteMe = document.querySelector("button")
+console.log)deleteMe
+
+const guests = [{id: 1,
+  name: "Guest Name",
+  email: "guest@email.com",
+  phone: "123-456-7890",
+   },
+  
+  {id: 2,
+  name: "CHEST NAME",
+  email: "chest@yahoo.com",
+  phone: "333-333-3333",},
+
+{id: 1,
+  name: "Guest Name",
+  email: "guest@email.com",
+  phone: "123-456-7890",
+  },
+
+  {id: 1,
+    name: "Guest Name",
+    email: "guest@email.com",
+    phone: "123-456-7890",
+    },
+
+    {id: 1,
+  name: "Guest Name",
+  email: "guest@email.com",
+  phone: "123-456-7890",},
+]
+  
+   
+  
+}
