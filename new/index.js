050fa10bed0458f702a7ac82b4afdd5e61c21007
Desean Richard <desean@Deseans-MacBook-Air.local>
2024-05-19 23:59:47 -0500

    “NEWUPDATE”

--- new/index.js
+++ new/index.js
@@ -1,19 +1,68 @@
-const API_URL =`https://fsa-crud-2aa9294fe819.herokuapp.com/api`
+const API_URL =`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2402-FTB-ET-WEB-FT/events`
 
-const fetchAPI = async () => {
+
+const partyForm = document.getElementById('partyForm');
+// const partyList = document.getElementById('partyList');
+
+const state = {
+  parties: [],
+}
+
+// const partyList = document.querySelector("#parties")
+
+// const addPartyForm = document.querySelector("#addParties");
+// addPartyForm.addEventListener("submit", addParty)
+
+// GET PARTIES MAYNE!
+
+const getParties = async () => {
 try {
   const response = await fetch(API_URL);
-  const dataObject = await response.json();
-
+  const result = await response.json();
+  console.log(result)
+}
+catch (error) {
+  throw error 
 }
 
 }
+
+//  RENDER PARTIES MAYNE!
+
+async function renderParties() {
+  state.parties.forEach((parties) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${parties.name} - ${parties.description}`;
+    partyList.appendChild(li);
+
+  })
+
+   await getParties();
+}
+
+getParties();
+
+
+
+
+//  POST THE PARTIES MAYNE!
+
+
+
+
+
+
+
+
+
+
+//  DELETE THE PARTIES MAYNE!
 
 const clickMe = document.querySelector("button1")
 console.log(clickMe)
 
 const deleteMe = document.querySelector("button")
-console.log)deleteMe
+console.log(deleteMe)
 
 const guests = [{id: 1,
   name: "Guest Name",
@@ -41,9 +90,12 @@ const guests = [{id: 1,
     {id: 1,
   name: "Guest Name",
   email: "guest@email.com",
-  phone: "123-456-7890",},
+  phone: "123-456-7890",}
 ]
   
+
+
+
    
   
-}
+
